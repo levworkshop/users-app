e51d5bcc6dbafa67bb4bd9287dc591222302120f
levworkshop <noreply@githubusers.com>
2023-05-01 18:37:22 +0300

    print users aray with map

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,35 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import Status from './components/Status';
 
+export interface UserTypes {
+    fullName: string;
+    status: "Active" | "Expired" | "Banned";
+    email: string;
+}
+
+const data: Array<UserTypes> = [
+    {
+        fullName: "Leeav",
+        status: 'Active',
+        email: "lee@getMaxListeners.com"
+    },
+    {
+        fullName: "Itai",
+        status: "Active",
+        email: "itai@getMaxListeners.com"
+    },
+    {
+        fullName: "Einat",
+        status: "Expired",
+        email: "Einat@getMaxListeners.com"
+    },
+    {
+        fullName: "Nati",
+        status: "Banned",
+        email: "nati@getMaxListeners.com"
+    }
+];
+
 function App() {
     return (
         <div className="bg-light m-4">
@@ -14,16 +43,15 @@ function App() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Mark Otto</td>
-                        <td><Status /></td>
-                        <td>mark@mdo.com</td>
-                    </tr>
-                    <tr>
-                        <td>Larisa Smith</td>
-                        <td><Status /></td>
-                        <td>larisa@mdo.com</td>
-                    </tr>
+                    {
+                        data.map(user =>
+                            <tr>
+                                <td>{user.fullName}</td>
+                                <td><Status /></td>
+                                <td>{user.email}</td>
+                            </tr>
+                        )
+                    }
                 </tbody>
             </table>
         </div>
