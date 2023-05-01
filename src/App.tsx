e10f3080f1e1f9c66cb91dfdef25562b89b5e885
levworkshop <noreply@githubusers.com>
2023-05-01 18:13:05 +0300

    static html

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,28 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
     return (
-        <div>
-
+        <div className="bg-light m-4">
+            <table className="table w-50 border border-dark table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Full Name</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark Otto</td>
+                        <td><span className="badge text-bg-success">Active</span></td>
+                        <td>mark@mdo.com</td>
+                    </tr>
+                    <tr>
+                        <td>Larisa Smith</td>
+                        <td><span className="badge text-bg-success">Active</span></td>
+                        <td>larisa@mdo.com</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
