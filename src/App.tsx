8fc53e8d757a4adc40050374b724c5b2dfc3ebff
levworkshop <noreply@githubusers.com>
2023-05-11 17:31:58 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
-import Status from './components/Status';
 import { useState } from 'react';
 import AddUserForm from './components/AddUserForm';
 import Table from './components/Table';
 
-// export type StatusType = "Active" | "Expired" | "Banned";
 export enum StatusEnum {
     active = 'active',
     expired = 'expired',
@@ -19,32 +17,6 @@ export interface UserTypes {
     email: string;
 }
 
-// const data: Array<UserTypes> = [
-//     {
-//         id: 1,
-//         fullName: "Leeav",
-//         status: StatusEnum.active,
-//         email: "lee@getMaxListeners.com"
-//     },
-//     {
-//         id: 2,
-//         fullName: "Itai",
-//         status: StatusEnum.active,
-//         email: "itai@getMaxListeners.com"
-//     },
-//     {
-//         id: 3,
-//         fullName: "Einat",
-//         status: StatusEnum.expired,
-//         email: "Einat@getMaxListeners.com"
-//     },
-//     {
-//         id: 4,
-//         fullName: "Nati",
-//         status: StatusEnum.banned,
-//         email: "nati@getMaxListeners.com"
-//     }
-// ];
 
 function App() {
     const [users, setUsers] = useState<Array<UserTypes>>([]);
